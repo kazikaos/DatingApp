d74050345e119009174873dd52bd4e5295e3b41b
Michael May <kazikaos@gmail.com>
2022-06-26 14:54:52 -0400

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities
+{
+	
+	public class AppUser
+	{
+        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
+
+
